52282c06f72e8dabf26367f5f6babec1f82eb3ab
RonGuymon <rnguymon@gmail.com>
2017-07-11 13:19:34 -0600

    Update to updateDashboard

--- R/updateDashboard.R
+++ R/updateDashboard.R
@@ -3,12 +3,13 @@
 #'
 #' This function uses the V2 version of the API.
 #' @param apiKey You can find the API key in the settings after logging into Numetric
+#' @param dashboardId The id of the dashboard. This can be found by using the getDashboards function, or by navigating to the dashboard and then selecting the string after the last forward slash.
 #' @param name The name of the dashboard, in quotes, in Numetric. Defaults to "None".
 #' @param category The category, in quotes, of the dashboard. Defaults to "None".
 #' @param content JSON formatted string of text. See https://nrl.readme.io/docs for more information.
 #' @export
-updateDashboard <- function(apiKey, datasetId, name = "None", category = "None", content){
-  r <- PATCH(paste0("https://api.numetric.com/v2/dashboard/", datasetId),
+updateDashboard <- function(apiKey, dashboardId, name = "None", category = "None", content){
+  r <- PATCH(paste0("https://api.numetric.com/v2/dashboard/", dashboardId),
              add_headers("Authorization" = apiKey,
                          "Content-Type" = "application/json"
              ),
