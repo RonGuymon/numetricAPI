4dc3ce0f74eeb58af9a0d0d8b8a35d755e36de31
RonGuymon <rnguymon@gmail.com>
2017-10-11 13:53:02 -0600

    Transformations update

--- R/getTableDetails.R
+++ R/getTableDetails.R
@@ -3,6 +3,7 @@
 #' @param apiKey You can find the API key in the settings after logging into Numetric
 #' @param tableId The id of the table. You can find this by using the getTables function, or by navigating to the table, and then selecting the string after the last forward slash of the url.
 #' @param fieldNames By default it returns the display name, order, and type of all the fields. This can make the table very wide. If set to false, this field information will not be returned.
+#' @return A list of attributes: accessedAt, accessedBy, acl, categories, createdAt, description, ext, id, name, org, updatedAt, updatedBy, version, ownerInfo, updatedByInfo, fields, primaryKey, transformations, active, stats, status, sourceId, options, schedule
 #' @export
 getTableDetails <- function(apiKey, tableId, fieldNames = T){
   r <- GET(paste0("https://api.numetric.com/v3/table/", tableId),
@@ -10,14 +11,15 @@ getTableDetails <- function(apiKey, tableId, fieldNames = T){
                        "Content-Type" = "application/json"),
            verbose()
   )
-  response <- httr::content(r, as = "text") # Saves what was returned as raw text with all the encodings
-  response <- fromJSON(response) # Converts what was returned to a dataframe
-  r2 <- list.flatten(response) %>% as.data.frame()
-  colnames(r2) <- gsub("\\.", "_", colnames(r2))
-  if(fieldNames == F){
-    r2 <- r2[,which(grepl("fields_", x = colnames(r2)) == F)]
-  }
-  return(r2)
+  response <- httr::content(r, as = "text") %>% # Saves what was returned as raw text with all the encodings
+    fromJSON(response) # Converts what was returned to a list or a dataframe
+  return(response)
+  # r2 <- list.flatten(response) %>% as.data.frame()
+  # colnames(r2) <- gsub("\\.", "_", colnames(r2))
+  # if(fieldNames == F){
+  #   r2 <- r2[,which(grepl("fields_", x = colnames(r2)) == F)]
+  # }
+  # return(r2)
 }
 
 
--- R/updateTable.R
+++ R/updateTable.R
@@ -2,65 +2,106 @@
 #' @description Updates a table using a dataframe.
 #'
 #' This function uses the V3 version of the API.
-#' @param apiKey You can find the API key in the settings after logging into Numetric
-#' @param tableId The id, in quotes, of a table in the Data Warehouse.
+#' @param apiKey Required. You can find the API key in the settings after logging into Numetric
+#' @param tableId Required. The id, in quotes, of a table in the Data Warehouse.
 #' @param numetricName The name, in quotes, given to the dataset in the Numetric Warehouse.
+#' @param description A description of the dataframe in quotes.
 #' @param dataframeName The name, without quotes, of the dataframe that will be used to create the dataset in Numetric.
 #' @param category The category, in quotes, given to the dataset in the Numetric Warehouse.
 #' @param primaryKey The name of the column, in quotes, that will be used as the primary key. Each row should have a unique id, otherwise, the last row indexed will be the one saved.
 #' @param geoshapes The column name, or vector of column names, in quotes, that will be stored as a geoShape (point on a map). This needs to be in the format, "lat,long".
 #' @param geopoints The column name, or vector of column names, in quotes, that will be stored as a geoPoint (for heatmaps). This needs to be in the format, "lat,long".
 #' @param boolean The column name, or vector of column names, in quotes, that will be stored as True/False. True = 1, False = 0.
+#' @param transformations A dataframe of transformations. Columns are: actionType, displayName, index, name, sourceField, targetField, type, and values. Values is a list of lists. It's easiest to use the getTable function on a table that already has transofrmations, and then look at the transformations part.
 #' @return Numetric Id
 #' @export
 
-updateTable <- function(apiKey, tableId, numetricName, dataframeName, category = "New Data", primaryKey, geoshapes = "", geopoints = "", boolean = ""){
+updateTable <- function(apiKey, tableId, numetricName, dataframeName, category, primaryKey, geoshapes = "", geopoints = "", boolean = "", transformations){
   # Example: nApiCreateTableV3(apiKey = apiKey, numetricName = "Retail Sports",dataframeName = sport2Sample,category = "Retail Sports", primaryKey="primaryKey")
   # Be sure to have a field in the dataframe that is a unique value for each row.
 
-  ## Create a dataframe with column attributes----
-  # Create a dataframe with columns for each field: field, displayName, autocomplete, and type.
-  fieldAttributes <- data.frame(field = colnames(dataframeName), # Gets the column names of the dataset to index
-                                displayName = colnames(dataframeName), # Uses the column names of the dataset as the display names
-                                # autocomplete = rep("false", ncol(dataframeName)), # Does not make anything autocompletable
-                                type = array(lapply(dataframeName, class)) %>% as.character() # Gets the class of each column in the dataframe
-  )
-  # Convert r class types to valid numetric class types
-  fieldAttributes <- mutate(fieldAttributes, type = ifelse(type == "character", "string",
-                                                           ifelse(type == "numeric", "double",
-                                                                  ifelse(type == "integer", "integer",
-                                                                         ifelse(type == "Date" | type == "POSIXct" | type == "POSIXlt", "datetime", "string")))))
+  if(exists("dataframeName")){
+    ## Create a dataframe with column attributes----
+    # Create a dataframe with columns for each field: field, displayName, autocomplete, and type.
+    fieldAttributes <- data.frame(field = colnames(dataframeName), # Gets the column names of the dataset to index
+                                  displayName = colnames(dataframeName), # Uses the column names of the dataset as the display names
+                                  # autocomplete = rep("false", ncol(dataframeName)), # Does not make anything autocompletable
+                                  type = array(lapply(dataframeName, class)) %>% as.character() # Gets the class of each column in the dataframe
+    )
+    # Convert r class types to valid numetric class types
+    fieldAttributes <- mutate(fieldAttributes, type = ifelse(type == "character", "string",
+                                                             ifelse(type == "numeric", "double",
+                                                                    ifelse(type == "integer", "integer",
+                                                                           ifelse(type == "Date" | type == "POSIXct" | type == "POSIXlt", "datetime", "string")))))
 
-  # Add in the fields that should be autocompleted and geocoded
-  fieldAttributes <- mutate(fieldAttributes,
-                            type = ifelse(field %in% geoshapes, "geo_shape",
-                                          ifelse(field %in% geopoints, "geo_point", type)),
-                            type = ifelse(grepl("date", field, ignore.case = T), "datetime", type),
-                            type = ifelse(field %in% boolean, "boolean", type)
-  )
-  wholeString <- ""
-  for (i in 1:nrow(fieldAttributes)){
+    # Add in the fields that should be autocompleted and geocoded
+    fieldAttributes <- mutate(fieldAttributes,
+                              type = ifelse(field %in% geoshapes, "geo_shape",
+                                            ifelse(field %in% geopoints, "geo_point", type)),
+                              type = ifelse(grepl("date", field, ignore.case = T), "datetime", type),
+                              type = ifelse(field %in% boolean, "boolean", type)
+    )
 
-    tempString <- paste0('"',fieldAttributes[i,1],'": {"displayName": "',fieldAttributes[i,2] ,'","type": "',fieldAttributes[i,3],'"},')
+    wholeString <- ""
+    for (i in 1:nrow(fieldAttributes)){
 
-    wholeString <- paste0(wholeString,tempString)
-    if(i == nrow(fieldAttributes)){
-      wholeString <- gsub(",$","",wholeString)
+      tempString <- paste0('"',fieldAttributes[i,1],'": {"displayName": "',fieldAttributes[i,2] ,'","type": "',fieldAttributes[i,3],'"},')
+
+      wholeString <- paste0(wholeString,tempString)
+      if(i == nrow(fieldAttributes)){
+        wholeString <- gsub(",$","",wholeString)
+      }
+
     }
 
+    fieldAttributesReady_body <- paste0('"fields": {',wholeString,'}')
+
+  }else{
+    fieldAttributesReady_body <- '"deleteMe"'
   }
 
-  fieldAttributesReady <- paste0("{",wholeString,"}")
+  if(exists("numetricName")){
+    numetricName_body <- paste0('"name": "', numetricName, '"')
+  }else{
+    numetricName_body <- '"deleteMe"'
+  }
+
+  if(exists("primaryKey")){
+    primaryKey_body <- paste0('"primaryKey": ["', primaryKey, '"]')
+  }else{
+    primaryKey_body <- '"deleteMe"'
+  }
+
+  if(exists("category")){
+    category_body <- paste0('"categories": ["', category, '"]')
+  }else{
+    category_body <- '"deleteMe"'
+  }
+
+  if(exists("description")){
+    description_body <- paste0('"description": "', description, '"')
+  }else{
+    description_body <- '"deleteMe"'
+  }
+
+  if(exists("transformations")){
+    transformations_body <- paste0('"transformations":', toJSON(transformations))
+  }else{
+    transformations_body <- '"deleteMe"'
+  }
 
   ## Merge the field attributes with the other information required----
-  metadata <- paste0('{"name": "', numetricName,
-                    '","primaryKey": ["', primaryKey,'"],
-                    "categories": ["', category,
-                    '"],"description": "Stuff indexed through R","fields":',
-                    fieldAttributesReady,
-                    '}'
-                    )
+  metadata <- paste(numetricName_body,
+                    primaryKey_body,
+                    category_body,
+                    description_body,
+                    fieldAttributesReady_body,
+                    transformations_body,
+                    sep = ",") %>%
+    gsub('"deleteMe",|"deleteMe"|,"deleteMe"', '', .) %>%
+    paste0('{', ., '}')
 
+  # Index the data----
   r <- PATCH(paste0("https://api.numetric.com/v3/table/", tableId),
             add_headers("Authorization" = apiKey,
                         "Content-Type" = "application/json"),
@@ -69,4 +110,32 @@ updateTable <- function(apiKey, tableId, numetricName, dataframeName, category =
   )
   content(r)
 }
+
+# library(httr)
+# library(dplyr)
+# library(magrittr)
+# library(jsonlite)
+# apiKey <- "SMGfRBqmXanyL9mCigflRpJUONSbHvxFxWS1Y3Y1sms%3D"
+# tableId <- "6f059e23-130b-4ec7-a186-cb4a7749c882" #Signbase2
+# # tableId <- "c1753796-83fa-4a65-aaa9-fd71d56462a9" # Signbase
+# category <- "Gnocchi"
+#
+# r <- GET(paste0("https://api.numetric.com/v3/table/", tableId),
+#          add_headers("Authorization" = apiKey,
+#                      "Content-Type" = "application/json"),
+#          verbose()
+# )
+# response <- httr::content(r, as = "text") %>% fromJSON()
+# response
+# trannies <- response$transformations
+# transformations <- trannies %>%
+#   mutate(
+#     sourceField = "L2DirOfTravel",
+#     targetField = "L2DirOfTravel",
+#     index = ifelse(index == 1, 3, 4)
+#   ) %>%
+#   bind_rows(trannies, .)
+
+
+
 
